f7db3b6f5548305ab234cf593e3904f2f85d3762
XuSog <1250021699@qq.com>
2023-10-21 17:28:14 +0800

    basic app config

---
+++ src/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAlW3O3ZHRM5crM-qLbfrJ6ppF5H_r-dEE",
+    authDomain: "tapau-ac329.firebaseapp.com",
+    projectId: "tapau-ac329",
+    storageBucket: "tapau-ac329.appspot.com",
+    messagingSenderId: "503400986804",
+    appId: "1:503400986804:web:d09913730e6778db107fe1",
+    measurementId: "G-PJCG018M6L"
+  };
+
+export default firebaseConfig;
